b0c57bd6f362cf482eb55e29e8a41c43156b90fc
unknown <Irina@irina.esed.kodeks.ru>
2015-03-17 12:06:54 +0300

    android 1.0.9

--- platforms/ios/www/js/interface.js
+++ platforms/ios/www/js/interface.js
@@ -7,7 +7,6 @@ function refreshBills() {
     }
     
     db.transaction(
-
     function(transaction) {
         transaction.executeSql('SELECT * FROM Bills;', [], function(transaction, result) {
         	 $('#billList').html('');
@@ -16,12 +15,6 @@ function refreshBills() {
             for (var i = 0; i < result.rows.length; i++) {
              d = new Date();
                 var row = result.rows.item(i);
-               // var newEntryRow = $('#entryTemplate').clone();
-               // newEntryRow.removeAttr('id');
-               // newEntryRow.removeAttr('style');
-               // newEntryRow.data('entryId', row.id);
-               // newEntryRow.appendTo('#patientList ul');
-               // newEntryRow.find('.label').text(row.pName);
                 $('#billList').append(
 		      	'<li onclick = "showCheck('+row.id+')">'
 		       +'<a href="#">'
@@ -59,6 +52,75 @@ function refreshBills() {
   
 }
 
+/*var expenses = {
+		"expenses":[
+		{"title": "День рождения",
+			"comment": "Билеты в парк отдыха на четверых",
+			"date": "06.12.2014",
+			"time": "14:17",
+			"cost": "4000"}
+		+"{\"title\": \"Игрушка\",comment: \"Купил игрушку для дочки\",date: \"13.11.2014\","
+		+"\"time\": \"12:28\",\"cost\": \"525\"},"
+
+		+"{\"title\": \"Бильярд\",\"comment\": \"Поход в бильярд на 1.5 часа\",\"date\": \"13.11.2014\","
+		+"\"time\": \"20:29\",\"cost\": \"1000\"},"
+
+		+"{\"title\": \"Поход в кино\",\"comment\": \"2 билета на фильм\",\"date\": \"10.11.2014\","
+		+"\"time\": \"19.30\",\"cost\": \"900\"}"
+
+		
+		]};*/
+
+ 
+
+var dictionary = {
+	 /*   "employee1":[
+	        {"id":"0","name":"Google"},
+	        {"id":"1","name":"eBay"}
+	    ],
+	    "employee2": [
+	        {"id":"2","name":"Yahoo"},
+	        {"id":"3","name":"Facebook"}
+	    ]*/
+	};
+
+function refreshExpenses(){
+	/*var json = '{"expenses":[{"title": "День рождения","comment": "Билеты в парк отдыха на четверых","date": "06.12.2014","time": "14:17","cost": "4000"}]}';
+    obj = JSON.parse(json);
+
+	alert(obj.count);
+	alert(obj.expenses.count);*/
+//	var exp = JSON.parse(dictionary);
+/*	for (var i=0; i<exp.length;i++){
+		alert(exp[i]);
+	}*/
+   /* for (var i = 0; i < result.rows.length; i++) {
+     d = new Date();
+        var row = result.rows.item(i);
+        $('#billList').append(
+      	'<li onclick = "showCheck('+row.id+')">'
+       +'<a href="#">'
+        +'<img src="'+row.path+"?"+d.getTime()+'" width = "70">'
+        +'<h4>'+row.name+" "+row.id+'</h4>'
+        +'<p>'+formatDate(row.createdate)+'</p>'
+        +'<p><img id="uploadImage'+row.id+'" width = "20"></img>'
+        +'<h5 id="uploadProgress'+row.id+'">asdfasd</h5>' 
+        +'</p>'
+        +'</a>'
+      +'</li>');
+        if (row.sent==1){
+        	 $('#uploadImage'+row.id).attr('src','img/cameraoverlay/send.png');
+        	 $('#uploadProgress'+row.id).hide();
+        }
+        else{
+        	$('#uploadImage'+row.id).attr('src','img/cameraoverlay/upload_blue.png');
+        	$('#uploadProgress'+row.id).hide();
+        }
+        
+       $('#billList').listview('refresh');
+    }
+	*/
+}
 
 function refershBillSendStatus(rowID){
     if (!db) return;
